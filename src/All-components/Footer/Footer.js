3121775f2359a75ff4a16b8e15027e845e6133e9
developer-sahadat <mdridoysikder862@gmail.com>
2022-03-19 02:01:05 +0600

    footer add

---
+++ src/All-components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='p-5 bg-dark text-light text-center'>
+           <p>Copyright © 2022 Online Shop</p>
+        </div>
+    );
+};
+
+export default Footer;
--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './All-components/Header/Header';
 import Main from './All-components/Main/Main';
 import { useState } from 'react';
+import Footer from './All-components/Footer/Footer';
 
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
     <div className='main-section'>
       <Header counts={count}></Header>
       <Main countUpdate={updateCount}></Main>
+      <Footer></Footer>
     </div>
   );
 }
